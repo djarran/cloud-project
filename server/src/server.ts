384e7f1ecd8d621a1e875be39429d154f9c27a12
djarran <me@djarran.com>
2023-08-28 18:35:18 +1000

    Working Docker Compose and Dockerfiles

--- server/src/server.ts
+++ server/src/server.ts
@@ -4,7 +4,7 @@ const app: Express = express();
 const port: number = 4000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.status(200).send("All good to go!");
+  res.status(200).send("Daaaaamn");
 });
 
 app.listen(port, () => {
