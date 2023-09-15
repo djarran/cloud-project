e63a23023ef292a8a409740dfc8f4a1fda7aecf8
djarran <me@djarran.com>
2023-09-15 16:49:05 +1000

    Remove createMessage from counter router

--- server/src/routes/counter.routes.ts
+++ server/src/routes/counter.routes.ts
@@ -8,7 +8,7 @@ const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
     const currentCounter = await getCurrentCounter();
-    await createMessage('Hey');
+    // await createMessage('Hey');
     // await getRandomYouTubeVideo();
 
     console.log(currentCounter)
