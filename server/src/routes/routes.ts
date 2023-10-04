03e9470a06c7c5e7c4be8d16eb8cf4ad0ac1fdd5
djarran <me@djarran.com>
2023-10-04 23:41:23 +1000

    Remove files from previous project

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -1,24 +1,9 @@
 import { Router } from "express";
-import { redditRouter } from "./reddit.routes.ts";
-import { addRouter } from "./add.routes.ts";
-import { counterRouter } from "./counter.routes.ts";
 import { apiRouter } from "./search.routes.ts";
 
 export const router: Router = Router();
 
 const defaultRoutes = [
-    {
-        path: '/reddit',
-        route: redditRouter
-    },
-    {
-        path: '/add',
-        route: addRouter
-    },
-    {
-        path: '/counter',
-        route: counterRouter
-    },
     {
         path: '/api',
         route: apiRouter
